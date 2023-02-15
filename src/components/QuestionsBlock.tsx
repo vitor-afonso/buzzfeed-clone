52a59e623c4666a7d4abe591a0c6c42608cd342a
Vitor Afonso <vittokm@hotmail.com>
2023-02-15 19:09:26 +0000

    components data completed

--- src/components/QuestionsBlock.tsx
+++ src/components/QuestionsBlock.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Content } from '../../interfaces';
+import { Content, Question } from '../../interfaces';
+import QuestionBlock from './QuestionBlock';
 
 const QuestionsBlock = ({ quizItem }: { quizItem: Content }) => {
   return (
     <>
       <h2 id={String(quizItem.id)}>{quizItem.text}</h2>
+
+      <div className='questions-container'>
+        {quizItem?.questions.map((question: Question, _index) => (
+          <QuestionBlock question={question} key={_index} />
+        ))}
+      </div>
     </>
   );
 };
